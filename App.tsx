290a51452a217f3536132544c754eeeeb059339b
victorandujar <victorandujarmurcia@gmail.com>
2023-05-03 16:41:05 +0200

    Create LoginForm component as static component

--- App.tsx
+++ App.tsx
@@ -1,15 +1,23 @@
 import { StyleSheet, View } from "react-native";
 import React from "react";
 import Header from "./src/components/Header/Header";
+import LoginForm from "./src/components/LoginForm/LoginForm";
+import globalStyles from "./src/styles/globalStyles";
 
 const App = (): JSX.Element => (
   <View style={styles.container}>
     <Header />
+    <View style={globalStyles.screen}>
+      <LoginForm />
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    flexDirection: "column",
+    gap: 300,
     backgroundColor: "#fff",
   },
 });
